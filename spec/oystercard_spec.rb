1e7168694b82ed1a74cd910429faff2f6a9fb118
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-26 13:39:31 +0100

    passed challenge 5: top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,14 @@
 require 'oystercard'
 
 describe OysterCard do
+
   it 'has a balance of zero' do
   	expect(subject.balance).to eq(0)
   end
 
-  
+  it { is_expected.to respond_to(:top_up).with(1).argument}
+
+  it "can top_up the balance" do
+    expect{subject.top_up(100)}.to change{ subject.balance }.to(100)
+  end
 end
