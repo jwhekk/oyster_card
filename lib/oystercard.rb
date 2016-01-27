0a728c0e0b31fc1a783b60afb035c54e03e6e560
Marco Bagnasco <admin@hecticmojo.com>
2016-01-27 15:08:40 +0000

    Adding hourney history feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,10 @@
 class OysterCard
  CARD_LIMIT = 90
  MIN_FARE = 1
- attr_reader :balance, :entry_station
+ attr_reader :balance, :entry_station, :history
  def initialize
    @balance = 0
+   @history = []
  end
 
  def top_up(value)
@@ -17,14 +18,19 @@ class OysterCard
   	raise "minimum fare is #{MIN_FARE} pound" if @balance < MIN_FARE
  end
 
- def touch_out
-  @entry_station = nil
- 	deduct(MIN_FARE)
+ def touch_out(station)
+   journey = {}
+   journey[@entry_station] = station
+   @history << journey
+   @entry_station = nil
+ 	 deduct(MIN_FARE)
+ 
  end
 
  def in_journey?
  	!!entry_station
  end
+ 
 
 private
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,13 @@
 require 'oystercard'
 
 describe OysterCard do
+  
+  
   let(:station) { double(:station)}
-	context "card balance" do
+	
+  context "card balance" do
+    
+    
 	  it 'has a balance of zero' do
 	  	expect(subject.balance).to eq(0)
 	  end
@@ -24,6 +29,7 @@ describe OysterCard do
     end
 
     it  { is_expected.to respond_to(:touch_in).with(1).argument }
+    it  { is_expected.to respond_to(:touch_out).with(1).argument }
 
 		it "stores the entry station" do
 			subject.touch_in(station)
@@ -32,7 +38,7 @@ describe OysterCard do
 
 		it "clears enty_station" do
 			subject.touch_in(station)
-			subject.touch_out
+			subject.touch_out(station)
 			expect(subject.entry_station).to eq nil
 		end
 
@@ -42,13 +48,24 @@ describe OysterCard do
 		end
 
 		it 'in_journey is false after touch out' do
-			subject.touch_out
+			subject.touch_out(station)
 			expect(subject).not_to be_in_journey
 		end
+    
+    it 'has an empty history when initialised' do
+      expect(subject.history).to eq []
+    end
+    
+    it 'stores the history of the journeys' do 
+      subject.touch_in("Victoria")
+      subject.touch_out("Kingston")
+      expect(subject.history).to eq [{"Victoria"=>"Kingston"}]
+      
+    end
 
     it 'deducts #{OysterCard::MIN_FARE} fare' do
 			subject.touch_in(station)
-			expect{subject.touch_out}.to change{subject.balance}.by(-1)
+			expect{subject.touch_out(station)}.to change{subject.balance}.by(-1)
 		end
 
 	end
